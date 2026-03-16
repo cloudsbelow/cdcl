daef9ddfc3c1771e3301f7cdc659ba14be187013
cloudsbelow <150115403+cloudsbelow@users.noreply.github.com>
2026-03-15 17:15:59 -0700

    example big

--- main.cpp
+++ main.cpp
@@ -14,26 +14,28 @@ void testWatchedLits(){
 }
 
 int main(){
-  testWatchedLits();
-  CNF cnf;
-  Clause c1 = {1};
-  Clause c2 = {-1, 2};
-  Clause c3 = {-3, 4};
-  Clause c4 = {-5, -6};
-  Clause c5 = {-1,-5,7};
-  Clause c6 = {-2,-5,6,-7};
-  cnf.addClause(c1);
-  cnf.addClause(c2);
-  cnf.addClause(c3);
-  cnf.addClause(c4);
-  cnf.addClause(c5);
-  cnf.addClause(c6);
-  CdclSolver solver(7, cnf);
+  // testWatchedLits();
+  // CNF cnf;
+  // Clause c1 = {1};
+  // Clause c2 = {-1, 2};
+  // Clause c3 = {-3, 4};
+  // Clause c4 = {-5, -6};
+  // Clause c5 = {-1,-5,7};
+  // Clause c6 = {-2,-5,6,-7};
+  // cnf.addClause(c1);
+  // cnf.addClause(c2);
+  // cnf.addClause(c3);
+  // cnf.addClause(c4);
+  // cnf.addClause(c5);
+  // cnf.addClause(c6);
+  // CdclSolver solver(7, cnf);
+  // std::cout << "Satisfiable: " << solver.solve() << std::endl;
+  // solver.printAssignment();
+  
+  CNF c;
+  ReadFromFile("cnfFiles/examples/CBS_k3_n100_m403_b10_0.cnf", c);
+  CdclSolver solver(110, c);
   std::cout << "Satisfiable: " << solver.solve() << std::endl;
   solver.printAssignment();
-  
-//   CNF c;
-//   ReadFromFile("cnfFiles/examples/CBS_k3_n100_m403_b10_0.cnf", c);
-//   printCnf(c);
 }
 
